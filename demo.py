69eecc92c114d6a8c58221916cf1b43d62557725
xiaowu <840017951@qq.com>
2019-10-14 16:32:55 +0800

    线程—v1

---
+++ demo.py
@@ -0,0 +1,26 @@
+import time
+import threading
+
+
+def sing():
+    for i in range(5):
+        print("千里香---%d" %i)
+        time.sleep(1)
+        
+
+def dance():
+    for i in range(5):
+        print("鬼步舞---%d" %i)
+        time.sleep(1)
+
+        
+def main():
+    t1 = threading.Thread(target=sing)
+    t2 = threading.Thread(target=dance)
+    
+    t1.start()
+    t2.start()
+
+
+if __name__ == "__main__":
+    main()
